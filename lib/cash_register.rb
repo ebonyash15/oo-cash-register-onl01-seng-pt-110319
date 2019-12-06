cb89887384de10ee6e0830159fe7ff87fbd547f6
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-06 00:08:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,13 @@
 class CashRegister
-    attr_accessor :discount
+  @@total=0
+  attr_accessor :discount
   def initialize(discount=0)
     @total = 0
+    @@total+=1
     @discount=discount
+  end
+  def self.total
+    @@total
   end
   def apply_discount
   end
